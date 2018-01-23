a3596e9bb6617dcc255708de373d6b7095a6702c
alperyilmazny <stfal1@gmail.com>
2018-01-23 10:14:58 -0500

    Minor refactoring

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,34 +1,39 @@
+function  calculateBatterySpanClassName(level) {
+    if (level >= 75 && level < 100) return "battery level-75";
+    if (level >= 50 && level < 75) return "battery level-50";
+    if (level >= 25 && level < 50) return "battery level-25";
+    if (level >= 0 && level < 25) return "battery level-0";
+    return "battery level-0";
+};
+
 var app = {
     
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+
+        // Add event listeners
+        window.addEventListener("batterystatus", this.onBatteryStatus, false);
+    },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-
-        // Add event listeners
-        window.addEventListener("batterystatus", this.onBatteryStatus, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-
-    calculateBatterySpanClassName1: function(level) {alert(level);
-        if (level >= 75 && level < 100) return "battery level-75";
-        if (level >= 50 && level < 75) return "battery level-50";
-        if (level >= 25 && level < 50) return "battery level-25";
-        if (level >= 0 && level < 25) return "battery level-0";
-
-        return "battery";
+        this.receivedEvent('deviceready');
     },
     
     onBatteryStatus: function (status) {
@@ -39,19 +44,8 @@ var app = {
         
         document.getElementById("batterySpan").className = className;
         document.getElementById("batteryPercent").innerHTML = status.level + " %";
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-
-        console.log('Received Event: ' + id);
     }
 };
 
-function  calculateBatterySpanClassName(level) {
-    if (level >= 75 && level < 100) return "battery level-75";
-    if (level >= 50 && level < 75) return "battery level-50";
-    if (level >= 25 && level < 50) return "battery level-25";
-    if (level >= 0 && level < 25) return "battery level-0";
-    return "battery level-0";
-};
+// Start the app
+app.initialize();
--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,26 @@
+function  calculateBatterySpanClassName(level) {
+    if (level >= 75 && level < 100) return "battery level-75";
+    if (level >= 50 && level < 75) return "battery level-50";
+    if (level >= 25 && level < 50) return "battery level-25";
+    if (level >= 0 && level < 25) return "battery level-0";
+    return "battery level-0";
+};
+
 var app = {
     
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+
+        // Add event listeners
+        window.addEventListener("batterystatus", this.onBatteryStatus, false);
+    },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -17,41 +34,18 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
-        
-        // Add event listeners
-        window.addEventListener("batterystatus", onBatteryStatus, false);
     },
     
-    // onBatteryStatus: function (status) {
-    //     var batteryLevel = (status && status.level) ? status.level : 0;
-    //     var isBatteryPlugged = (status && status.isPlugged) ? status.isPlugged : false;
+    onBatteryStatus: function (status) {
+        var batteryLevel = (status && status.level) ? status.level : 0;
+        var isBatteryPlugged = (status && status.isPlugged) ? status.isPlugged : false;
         
-    //     var className = calculateBatterySpanClassName(batteryLevel);
+        var className = calculateBatterySpanClassName(batteryLevel);
         
-    //     document.getElementById("batterySpan").className = className;
-    //     document.getElementById("batteryPercent").innerHTML = status.level + " %";
-    // },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        console.log('Received Event: ' + id);
+        document.getElementById("batterySpan").className = className;
+        document.getElementById("batteryPercent").innerHTML = status.level + " %";
     }
 };
 
-function  calculateBatterySpanClassName(level) {
-    if (level >= 75 && level < 100) return "battery level-75";
-    if (level >= 50 && level < 75) return "battery level-50";
-    if (level >= 25 && level < 50) return "battery level-25";
-    if (level >= 0 && level < 25) return "battery level-0";
-    return "battery level-0";
-};
-
-function onBatteryStatus(status) {
-    var batteryLevel = (status && status.level) ? status.level : 0;
-    var isBatteryPlugged = (status && status.isPlugged) ? status.isPlugged : false;
-    
-    var className = calculateBatterySpanClassName(batteryLevel);
-    
-    document.getElementById("batterySpan").className = className;
-    document.getElementById("batteryPercent").innerHTML = status.level + " %";
-},
+// Start the app
+app.initialize();
